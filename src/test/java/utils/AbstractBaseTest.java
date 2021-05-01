390d1c177fcb4aa644f37314d107ec45ad88344b
Cecilia <cecy.of@gmail.com>
2021-05-01 12:25:22 -0500

    Adding POM, Driver class, TestNG file

---
+++ src/test/java/utils/AbstractBaseTest.java
@@ -0,0 +1,26 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class AbstractBaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void cleanTest(){
+        //driver.quit();
+    }
+}
